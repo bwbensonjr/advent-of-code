7efc32a0c876ac8233958247d6456b2d8a4d8524
Brent Benson <bwbensonjr@gmail.com>
2024-12-02 07:40:41 -0500

    Comment code

--- 2024/day-01/distance.py
+++ 2024/day-01/distance.py
@@ -1,19 +1,31 @@
+# https://adventofcode.com/2024/day/1
+
 import pandas as pd
 import sys
 
 def main():
     in_file = sys.argv[1]
+
+    # Use pandas to parse the two columns of
+    # numbers separated by spaces.
     in_df = pd.read_table(
         in_file,
         names=["list_1", "list_2"],
         sep="  ",
         engine="python",
     )
+
+    # Create a new dataframe with the columns
+    # in sorted order.
     df = pd.DataFrame({
         "list_1": list(in_df.list_1.sort_values()),
         "list_2": list(in_df.list_2.sort_values()),
     })
+
+    # Create `distance` column with row-wise abs differences
     df["distance"] = abs(df["list_1"] - df["list_2"])
+
+    # Output value is sum of distances
     total_distance = df["distance"].sum()
     print(total_distance)
 
--- 2024/day-01/similarity.py
+++ 2024/day-01/similarity.py
@@ -1,19 +1,31 @@
+# https://adventofcode.com/2024/day/1
+
 from collections import Counter
 import pandas as pd
 import sys
 
 def main():
     in_file = sys.argv[1]
+
+    # Use pandas to read number columns into
+    # a DataFrame.
     in_df = pd.read_table(
         in_file,
         names=["list_1", "list_2"],
         sep="  ",
         engine="python",
     )
-    similarity = 0
+
+    # `collections.Counter` is great for counting
+    # occurences; used for second column.
     list_2_counts = Counter(in_df.list_2)
+    
+    # Iterate over first column multiplying the
+    # number by its count in second column.
+    similarity = 0
     for list_1_val in in_df.list_1:
         similarity += list_1_val * list_2_counts[list_1_val]
+        
     print(similarity)
 
 if __name__ == "__main__":
